f7d35b8f5cd580daa8501161c33a55f209c8ac30
Sabio Bezus Alejandro German <115056374+alesabio42@users.noreply.github.com>
2023-12-19 12:47:21 -0300

    Prueba PASO 6 - OK

--- 3. Script de Implementación/PASO 6 - Implementación de Sentencias.sql
+++ 3. Script de Implementación/PASO 6 - Implementación de Sentencias.sql
@@ -1,20 +1,20 @@
--- CREACIÓN DE USUARIO PARA SOLO LECTURA
+-- PASO 1 - CREACIÓN DE USUARIO PARA SOLO LECTURA
 CREATE USER 'usuario_lectura'@'localhost' IDENTIFIED BY 'coderhouse';
--- Asignar permisos de solo lectura a todas las tablas de la base de datos
+-- PASO 2 - Asignar permisos de solo lectura a todas las tablas de la base de datos
 GRANT SELECT ON *.* TO 'usuario_lectura'@'localhost';
 
--- CREACIÓN DE USUARIO PARA LECTURA, INSERCION Y MODIFICACIÓN
+-- PASO 3 - CREACIÓN DE USUARIO PARA LECTURA, INSERCION Y MODIFICACIÓN
 CREATE USER 'usuario_modificacion'@'localhost' IDENTIFIED BY 'coderhouse';
--- Asignar permisos de lectura, inserción y modificación a todas las tablas de la base de datos
+-- PASO 4 - Asignar permisos de lectura, inserción y modificación a todas las tablas de la base de datos
 GRANT SELECT, INSERT, UPDATE ON *.* TO 'usuario_modificacion'@'localhost';
 
--- No permitir la eliminación de registros para ambos usuarios
+-- PASO 6 - No permitir la eliminación de registros para ambos usuarios
 REVOKE DELETE ON *.* FROM 'usuario_lectura'@'localhost';
 REVOKE DELETE ON *.* FROM 'usuario_modificacion'@'localhost';
 
--- Asegurar que los cambios tengan efecto
+-- PASO 7 - Asegurar que los cambios tengan efecto
 FLUSH PRIVILEGES;
 
--- VERIFICACIÓN
+-- PASO 8 - VERIFICACIÓN
 SELECT * FROM mysql.user WHERE User = 'usuario_lectura';
 SELECT * FROM mysql.user WHERE User = 'usuario_modificacion';
